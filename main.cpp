141cf5b8849b290878c21e5a15256f9b05382469
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2024-04-29 20:05:14 -0400

    Added process class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <thread>
 #include <chrono>
+#include <vector>
 #include <iostream>
 
-class Resourse {
+
+class Resource {
 public:
     int number = 0;
     bool in_use = false;
@@ -11,26 +13,58 @@ public:
         if (in_use == true) {
             std::cout << "Race condition" << "\n";
         }
+
         in_use = true;
-        //std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         ++number;
         in_use = false;
     }
 };
 
-void use_resourse(Resourse& resourse) {
-    resourse.increment();
+
+class Process {
+public:
+    Resource& resource;
+    Process(Resource& resource_) : resource(resource_) {}
+
+    void sleep() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    void loop() {
+        // request to use resource once in a while
+        resource.increment();
+    }
+
+    void send_message() {
+        // send a message to another process
+    }
+
+    void request_resource() {
+        // ...
+    }
+
+    void release_resource() {
+        // ...
+    }
+};
+
+
+void create_process(Resource& resource) {
+    Process process(resource);
+    process.loop();
 }
 
+
 int main() {
-    Resourse resourse;
+    Resource resource;
 
     for (int i = 0; i < 5; ++i) {
-        std::thread t(use_resourse, std::ref(resourse));
+        std::thread t(create_process, std::ref(resource));
         t.detach();
     }
 
     std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    std::cout << resourse.number << "\n";
+    std::cout << resource.number << "\n";
 }
