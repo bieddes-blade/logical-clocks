8ecd22f034089811c32055f4da64a2e74dc4fbfa
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2024-04-29 20:50:33 -0400

    Added a message queue

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <map>
+#include <queue>
 #include <thread>
 #include <chrono>
 #include <vector>
@@ -24,24 +26,39 @@ public:
 
 class Process {
 public:
+    int my_id;
     Resource& resource;
-    Process(Resource& resource_) : resource(resource_) {}
+    std::map<int, std::queue<int>>& queue;
+
+    Process(int my_id_, Resource& resource_, std::map<int, std::queue<int>>& queue_) 
+        : my_id(my_id_), resource(resource_), queue(queue_) {}
 
     void sleep() {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     void loop() {
-        // request to use resource once in a while
-        resource.increment();
+        request_resource();
+        if (my_id == 0) {
+            send_message(4);
+        }
+        sleep();
+        check_new_messages();
     }
 
-    void send_message() {
-        // send a message to another process
+    void send_message(int other_id) {
+        queue[other_id].push(my_id);
+        std::cout << "Thread " << my_id << " sent a message" << "\n";
+    }
+
+    void check_new_messages() {
+        if (!queue[my_id].empty()) {
+            std::cout << "Thread " << my_id << " received a message" << "\n";
+        }
     }
 
     void request_resource() {
-        // ...
+        resource.increment();
     }
 
     void release_resource() {
@@ -50,17 +67,18 @@ public:
 };
 
 
-void create_process(Resource& resource) {
-    Process process(resource);
+void create_process(int id, Resource& resource, std::map<int, std::queue<int>>& queue) {
+    Process process(id, resource, queue);
     process.loop();
 }
 
 
 int main() {
     Resource resource;
+    std::map<int, std::queue<int>> queue;
 
     for (int i = 0; i < 5; ++i) {
-        std::thread t(create_process, std::ref(resource));
+        std::thread t(create_process, i, std::ref(resource), std::ref(queue));
         t.detach();
     }
 
