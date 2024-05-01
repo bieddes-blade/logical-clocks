23d2b05476bc270a8953977220a238f243c74b53
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2024-05-01 00:33:46 -0400

    Debugged :)

--- main.cpp
+++ main.cpp
@@ -13,9 +13,11 @@
 #include <mutex>
 #include <condition_variable>
 
+#include "SafeQueue.hpp"
 
-int NUM_THREADS = 2;
-int NUM_WRITES = 1;
+
+int NUM_THREADS = 3;
+int NUM_WRITES = 3;
 
 
 void sleep(int ms) {
@@ -29,6 +31,7 @@ public:
     int number;
     int timestamp;
     int type; // 1 == request resource, 2 == release resource, 3 == acknowledgement
+    Message() : from(-1), number(-1), timestamp(-1), type(-1) {}
     Message(int from_, int number_, int timestamp_, int type_)
         : from(from_), number(number_), timestamp(timestamp_), type(type_) {}
 };
@@ -48,55 +51,13 @@ public:
 };
 
 
-template<typename T>
-class ThreadsafeQueue {
-    std::queue<T> queue_;
-    mutable std::mutex mutex_;
-    bool empty() const {
-        return queue_.empty();
-    }
-
- public:
-    ThreadsafeQueue() = default;
-    ThreadsafeQueue(const ThreadsafeQueue<T> &) = delete ;
-    ThreadsafeQueue& operator=(const ThreadsafeQueue<T> &) = delete ;
- 
-    ThreadsafeQueue(ThreadsafeQueue<T>&& other) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        queue_ = std::move(other.queue_);
-    }
-
-    virtual ~ThreadsafeQueue() { }
-
-    unsigned long size() const {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return queue_.size();
-    }
-
-    std::optional<T> pop() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (queue_.empty()) {
-            return {};
-        }
-        T tmp = queue_.front();
-        queue_.pop();
-        return tmp;
-    }
-
-    void push(const T &item) {
-        std::lock_guard<std::mutex> lock(mutex_);
-        queue_.push(item);
-    }
-};
-
-
 class Process {
 public:
     int my_id;
     bool asking_for_resource = false;
     int writes = NUM_WRITES; // how many times this process will write into the resource
     Resource& resource;
-    std::map<int, ThreadsafeQueue<Message>>& queue;
+    std::map<int, SafeQueue<Message>>& queue;
     int clock = 0;
 
     std::map<int, int> sent_count; // how many messages this process has sent
@@ -120,7 +81,7 @@ public:
     };
     std::map<int, std::set<Message, cmp_number>> backlog; // messages that were received in the wrong order
 
-    Process(int my_id_, Resource& resource_, std::map<int, ThreadsafeQueue<Message>>& queue_) 
+    Process(int my_id_, Resource& resource_, std::map<int, SafeQueue<Message>>& queue_) 
         : my_id(my_id_), resource(resource_), queue(queue_) {}
 
     bool backlog_not_empty() {
@@ -149,16 +110,17 @@ public:
     }
 
     void send_message(int other_id, int type) {
+        sleep(20);
+
         if (sent_count.find(other_id) == sent_count.end()) {
             sent_count[other_id] = 1;
         } else {
             ++sent_count[other_id];
         }
-        int sz = queue[other_id].size();
-        queue[other_id].push(Message(my_id, sent_count[other_id], clock, type));
-        if (queue[other_id].size() <= sz) {
-            std::cout << "WTF\n";
-        }
+
+        Message new_message = Message(my_id, sent_count[other_id], clock, type);
+        queue[other_id].Produce(std::move(new_message));
+
         std::cout << "Thread " + std::to_string(my_id) + " sent message type " + std::to_string(type) + " to " + 
             std::to_string(other_id) + " at " + std::to_string(clock) + "\n";
         ++clock;
@@ -170,7 +132,8 @@ public:
         max_timestamp[sender] = std::max(max_timestamp[sender], message.timestamp);
 
         if (message.type == 1) { // processing a request resource message
-            requests.insert(message);
+            Message new_message = message;
+            requests.insert(new_message);
             send_message(sender, 3); // send acknowledgement message to sender
         } else if (message.type == 2) {
             // erase one previous request resource message from the sender
@@ -185,12 +148,8 @@ public:
     }
 
     void receive_new_messages() {
-        sleep(20);
-
-        std::optional<Message> optional_message = queue[my_id].pop();
-        while (optional_message) {
-            Message message = *optional_message;
-
+        Message message;
+        while (queue[my_id].Consume(message)) {
             int sender = message.from;
             clock = std::max(clock + 1, message.timestamp + 1);
 
@@ -202,36 +161,7 @@ public:
                 processed_count[sender] = 0;
                 max_timestamp[sender] = -1;
             }
-            
-            if (message.number > processed_count[sender] + 1) {
-                // the previous messages from this sender weren't received yet
-                backlog[sender].insert(message);
-                std::cout << "Thread " + std::to_string(my_id) + " placed message from " + std::to_string(sender) + " at " + 
-                    std::to_string(clock) + " into the backlog\n";
-            } else {
-                // the number of this message == (the number of the last processed message + 1)
-                process_message(message);
-
-                // lets go through the messages received in the wrong order and check if any can be processed now
-                for (auto it = backlog[sender].begin(); it != backlog[sender].end(); ) {
-                    Message current = *it;
-                    if (current.number == processed_count[sender] + 1) {
-                        // found next consecutive message
-                        process_message(current);
-                        std::cout << "Thread " + std::to_string(my_id) + " processed message from " + std::to_string(current.from) +  
-                            " at " + std::to_string(current.timestamp) + " from the backlog\n";
-                        it = backlog[sender].erase(it);
-                    } else {
-                        ++it;
-                    }
-                }
-            }
-
-            optional_message = queue[my_id].pop();
-            if (!optional_message) {
-                sleep(50);
-                optional_message = queue[my_id].pop();
-            }
+            process_message(message);
         }
     }
 
@@ -291,7 +221,7 @@ public:
 };
 
 
-void create_process(int id, Resource& resource, std::map<int, ThreadsafeQueue<Message>>& queue) {
+void create_process(int id, Resource& resource, std::map<int, SafeQueue<Message>>& queue) {
     Process process(id, resource, queue);
     process.loop();
 }
@@ -300,7 +230,7 @@ void create_process(int id, Resource& resource, std::map<int, ThreadsafeQueue<Me
 int main() {
     std::srand(std::time(nullptr));
     Resource resource;
-    std::map<int, ThreadsafeQueue<Message>> queue;
+    std::map<int, SafeQueue<Message>> queue;
     std::vector<std::thread> threads;
 
     for (int i = 0; i < NUM_THREADS; ++i) {
